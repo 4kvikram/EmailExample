84d91031f8d0634dfc980be30655c20aa526fe10
AnsitDevFive1 <>
2020-01-17 16:27:40 +0530

    first commit

---
+++ SendEmailExample/DashBoard.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DashBoard.aspx.cs" Inherits="SendEmailExample.DashBoard" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table align="center">
+                <tr>
+                    <td>To</td>
+                    <td><asp:TextBox ID="txtTo" runat="server" ></asp:TextBox> </td>
+                </tr>
+                <tr>
+                    <td>CC</td>
+                    <td><asp:TextBox ID="txtCC" runat="server" ></asp:TextBox> </td>
+                </tr>
+                <tr>
+                    <td>BCC</td>
+                    <td><asp:TextBox ID="txtBCC" runat="server" ></asp:TextBox> </td>
+                </tr>
+                <tr>
+                    <td>Subject</td>
+                    <td><asp:TextBox ID="txtSubject" runat="server" ></asp:TextBox> </td>
+                </tr>
+                 <tr>
+                    <td>Body</td>
+                    <td><asp:TextBox ID="txtBody" runat="server" ></asp:TextBox> </td>
+                </tr>
+                  <tr>
+                    <td></td>
+                    <td><asp:Button ID="btnSend" Text="Send Mail" runat="server" OnClick="btnSend_Click"/> </td>
+                </tr>
+                 <tr>
+                    <td></td>
+                    <td><asp:Label ID="lblmessage" runat="server"></asp:Label> </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ SendEmailExample/DashBoard.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace SendEmailExample
+{
+    public partial class DashBoard : System.Web.UI.Page
+    {
+
+        /*
+        Example:-
+       ===============================================================
+         <appSettings>
+             <add key="FromEmail" value="abcd@email.com" />
+         </appSettings>
+         ====================================================================
+         use here as :-
+         =====================================================================
+          string FromEmail = ConfigurationManager.AppSettings["FromEmail"];
+          =======================================================
+      */
+        //for testing enter Your Email Details here
+        // you should Add these Details in config file (as appSetting) as above
+
+        string PortNo = "Port";
+        string FromEmail = "fromEmail";
+        string FromPassword = "password";
+        string Host = "Host";
+     
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void SendEmail()
+        {
+            try
+            {
+                //get values from aspx
+                string EmaiilTo = txtTo.Text;
+                string CC = txtCC.Text;
+                string BCC = txtBCC.Text;
+                string EmailSubject = txtSubject.Text;
+                string EmailBody = txtBody.Text;
+                //=====
+                MailMessage mailMessage = new MailMessage(FromEmail, EmaiilTo, EmailSubject, EmailBody);
+                if (!string.IsNullOrEmpty(CC))
+                {
+                    mailMessage.CC.Add(CC);
+                }
+                if (!string.IsNullOrEmpty(BCC))
+                {
+                    mailMessage.Bcc.Add(BCC);
+                }
+                mailMessage.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = Host;
+                NetworkCredential networkCredential = new NetworkCredential(FromEmail, FromPassword);
+                smtp.Credentials = networkCredential;
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Port = Convert.ToInt32(PortNo);
+                //finally send
+                smtp.Send(mailMessage);
+                lblmessage.Text = "Message Send";
+            }
+            catch (Exception)
+            {  
+                     lblmessage.Text = "Message Not Send"; 
+            }
+        }
+        protected void btnSend_Click(object sender, EventArgs e)
+        {
+            SendEmail();
+        }
+    }
+}
